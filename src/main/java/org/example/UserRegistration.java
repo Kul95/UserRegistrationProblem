155441dc9e68d544d281b1663fa5860b6573a25b
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2023-12-06 10:43:06 +0530

    Added[Kuldeep]:UserRegistration lastName

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -3,10 +3,17 @@ package org.example;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean firstNameValidation(String firstName) throws Exception {
+    public boolean firstName(String firstName) throws Exception {
         if (Pattern.matches("[A-Z][a-z]{2,}", firstName))
             return true;
         else
             throw new Exception("You enter Invalid First Name");
+    }
+
+    public boolean lastName(String lastName) throws Exception {
+        if (Pattern.matches("[A-Z][a-z]{2,}", lastName))
+            return true;
+        else
+            throw new Exception("You enter Invalid Last Name");
     }
 }
--- src/test/java/org/example/UserRegistrationTest.java
+++ src/test/java/org/example/UserRegistrationTest.java
@@ -5,12 +5,24 @@ import org.junit.Test;
 public class UserRegistrationTest {
     UserRegistration userRegistration=new UserRegistration();
     @Test
-    public void validateFirstNameTestForPositive() {
+    public void testValidateFirstNameTestForPositive() {
         boolean name = false;
         try {
-            name = userRegistration.firstNameValidation("Kuldeep");
+            name = userRegistration.firstName("Kuldeep");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void validateLastName() {
+        boolean lastName = false;
+        try {
+            lastName = userRegistration.lastName("Kumar");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
